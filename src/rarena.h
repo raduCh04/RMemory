9d060ee87f9d9415e1c8e055237c5981b876acf1
RaduCh04 <chiraradu2004@gmail.com>
2024-10-15 17:57:10 +0200

    Add custom allocator support

--- src/rarena.h
+++ src/rarena.h
@@ -4,37 +4,73 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct arena
+typedef void *(*allocate)(size_t size);
+typedef void (*deallocate)(void *data);
+typedef struct Arena
 {
+    struct Arena *next_reg;
+    allocate allocate_func;
+    deallocate deallocate_func;
+    size_t alignment;
     size_t capacity;
     size_t size;
     uint8_t *data;
-} arena;
+} Arena;
 
-arena arena_init(size_t capacity)
+#ifndef RMEMDEF
+    #ifndef RMEM_STATIC
+        #define RMEMDEF extern
+    #else
+        #define RMEMDEF static
+    #endif
+#endif
+
+//NOTE: If no custom allocator, pass NULL for both allocate and deallocate
+RMEMDEF Arena arena_init(size_t capacity, size_t alignment, allocate allocate_func, deallocate deallocate_func);
+RMEMDEF void *arena_alloc(Arena *self, size_t size);
+inline RMEMDEF void arena_clear(Arena *self);
+RMEMDEF void arena_destroy(Arena *self);
+
+#ifdef RARENA_IMPLEMENTATION
+RMEMDEF Arena arena_init(size_t capacity, size_t alignment, allocate allocate_func, deallocate deallocate_func)
 {
-    arena result = {0};
+    Arena result = {0};
+    result.allocate_func = allocate_func ? allocate_func : malloc; 
+    result.deallocate_func = deallocate_func ? deallocate_func : free;
+    result.alignment = (alignment > 0) ? alignment : 1;
     result.capacity = capacity;
-    result.data = malloc(sizeof(uint8_t) * capacity);
+    result.data = result.allocate_func(sizeof(uint8_t) * capacity);
+    if (result.data == NULL)
+        assert(0 && "Failed to allocate memory!");
+    assert((self->alignment & (self->alignment - 1)) == 0 && "Alignment must be a power of 2.");
+
     return result;
 }
 
-void arena_alloc(arena *self, size_t size)
+static size_t align_(Arena *self)
 {
-    assert(self->size + size <= self->capacity);
-    self->size += size;
-    return &data[self->size];
+    return (self->size + self->alignment - 1) & ~(self->alignment - 1); 
 }
 
-void arena_clear(arena *self)
+RMEMDEF void *arena_alloc(Arena *self, size_t size)
+{
+    //TODO: Add overflow handling
+    size_t current_offset = align_(self);
+    assert(current_offset + size <= self->capacity);
+    self->size += current_offset + size;
+    return &self->data[current_offset];
+}
+
+inline RMEMDEF void arena_clear(Arena *self)
 {
     self->size = 0;
 }
 
-void arena_destroy(arena *self)
+RMEMDEF void arena_destroy(Arena *self)
 {
     self->capacity = 0;
     self->size = 0;
     if (self->data != NULL)
-        free(self->data);
+        self->deallocate_func(self->data);
 }
+#endif
